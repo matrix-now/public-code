0f03f544d89f8626b0ed7a3c227d3e1607dfeaf4
the.matrix <dev.snoopy@gmail.com>
2023-04-10 14:37:08 +0900

    테스트 중

--- src/main/java/io/sinsabridge/flowerbackend/FlowerBackendApplication.java
+++ src/main/java/io/sinsabridge/flowerbackend/FlowerBackendApplication.java
@@ -1,9 +1,11 @@
 package io.sinsabridge.flowerbackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "io.sinsabridge.flowerbackend.board.mapper", sqlSessionTemplateRef = "sqlSessionTemplate")
 public class FlowerBackendApplication {
 
     public static void main(String[] args) {
